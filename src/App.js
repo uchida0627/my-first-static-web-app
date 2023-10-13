76b33221315df94703bf0b7cc5f39923c5c55961
uchida0627 <97079120+uchida0627@users.noreply.github.com>
2023-10-13 11:35:48 +0900

    Update App.js
    change app.js line8

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   useEffect(() => {
     (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
+      const { text } = await( await fetch(`/api/src/message`)).json();
       setData(text);
     })();
   });
