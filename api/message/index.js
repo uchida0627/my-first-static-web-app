24978510211f6b583f08d4fe8cc1e9356a3ca46b
uchida0627 <v-tuchida@microsoft.com>
2024-01-04 17:16:58 +0900

    edit message / index.js

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,10 @@
+
 module.exports = async function (context, req) {
+    const Chara = ["A","B","C","D","E"]
+    let rundomSL = Math.floor(Math.random()*SLName.length)
+
+
     context.res.json({
-        text: "Hello from the Backend API"
+        text: "Hello from the Backend API.生成された文字は" + SLName[rundomSL] + "です。" 
     });
 };
